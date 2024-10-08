e625bb76d9f95d35a43659a1a62069bc4c6a49a6
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2024-10-08 14:27:05 -0400

    LL template.

--- src/main/java/com/cameodud/camlinkedlist/CamLinkedList.java
+++ src/main/java/com/cameodud/camlinkedlist/CamLinkedList.java
@@ -1,8 +1,33 @@
 package com.cameodud.camlinkedlist;
 
-
+// Code for a doubly linked-list
 public class CamLinkedList {
 	
+	public CamLinkedList() {
+		// your code here
+	}
 	
+	public int get(int i) {
+		// your code here
+		return 0;
+	}
+	
+	public void insertHead(int val) {
+		// your code here
+	}
+	
+	public void insertTail(int val) {
+		// your code here
+	}
+	
+	public boolean remove(int i) {
+		// your code here
+		return false;
+	}
+	
+	public int[] getValues() {
+		// your code here
+		return null;
+	}
 	
 }

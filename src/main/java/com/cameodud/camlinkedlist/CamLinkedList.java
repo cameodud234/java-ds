78fb59b5e02add74da4967cf754917de1fb1d089
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2024-10-08 17:49:04 -0400

    Update to the linkedlist impl.

--- src/main/java/com/cameodud/camlinkedlist/CamLinkedList.java
+++ src/main/java/com/cameodud/camlinkedlist/CamLinkedList.java
@@ -1,33 +1,102 @@
 package com.cameodud.camlinkedlist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Code for a doubly linked-list
 public class CamLinkedList {
 	
+	private Node head;
+	private Node tail;
+	
+	
 	public CamLinkedList() {
-		// your code here
+		head = new Node();
+		tail = new Node();
+		head.next = tail;
+		tail.prev = head;
 	}
 	
 	public int get(int i) {
-		// your code here
-		return 0;
+		int count = 0;
+		Node current = head.next;
+		while(current != null) {
+			if(count == i) {
+				return current.val;
+			}
+			current = current.next;
+		}
+		return -1;
 	}
 	
 	public void insertHead(int val) {
-		// your code here
+		Node begin = head;
+		Node afterBegin = head.next;
+		Node newNode = new Node(val, begin, afterBegin);
+		begin.next = newNode;
+		afterBegin.prev = newNode;
 	}
 	
 	public void insertTail(int val) {
-		// your code here
+		Node end = tail;
+		Node beforeEnd = tail.prev;
+		Node newNode = new Node(val, beforeEnd, end);
+		end.prev = newNode;
+		beforeEnd.next = newNode;
 	}
 	
 	public boolean remove(int i) {
-		// your code here
+		Node current = head.next;
+		int count = 0;
+		
+		while(current != null && current != tail) {
+			if(count == i) {
+				current.prev.next = current.next;
+				current.next.prev = current.prev;
+				return true;
+			}
+			current = current.next;
+			count++;
+		}
+		
 		return false;
+		
 	}
 	
-	public int[] getValues() {
-		// your code here
-		return null;
+	public List<Integer> getValues() {
+		Node current = head.next;
+		List<Integer> values = new ArrayList<>();
+		while(current != null && current != tail) {
+			values.add(current.val);
+			current = current.next;
+		}
+		return values;
+	}
+	
+	private class Node {
+		
+		public int val;
+		public Node prev;
+		public Node next;
+		
+		public Node() {
+			val = Integer.MIN_VALUE;
+			prev = null;
+			next = null;
+		}
+		
+		public Node(int val) {
+			this.val = val;
+			prev = null;
+			next = null;
+		}
+		
+		public Node(int val, Node prev, Node next) {
+			this.val = val;
+			this.prev = prev;
+			this.next = next;
+		}
+		
 	}
 	
 }

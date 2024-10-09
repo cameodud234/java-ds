ca6f055c1acae220277c727aa101fcf4dba91284
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2024-10-09 08:53:40 -0400

    Skeleton for Binary Search Tree

---
+++ src/main/java/com/cameodud/cambst/CamBST.java
@@ -0,0 +1,51 @@
+package com.cameodud.cambst;
+
+public class CamBST {
+	
+	public CamBST() {
+	}
+	
+	public CamBST(int val) {
+	}
+	
+	// Searches for element in tree.
+	public boolean search(int val) {
+		return false;
+	}
+	
+	// Removes element from tree
+	public boolean remove(int val) {
+		return true;
+	}
+	
+	// Shows elements in BST
+	public void traverse(int val) {
+		
+	}
+	
+	// Finds smallest element
+	public int getMin() {
+		return 0;
+	}
+	
+	// Finds largest element
+	public int getMax() {
+		return 0;
+	}
+	
+	// Finds height of bst.
+	public int getHeight() {
+		return 0;
+	}
+	
+	// Counts total node in tree
+	public int size() {
+		return 0;
+	}
+	
+	// Find Kth largest element
+	public int kLargest(int k) {
+		return 0;
+	}
+
+}
